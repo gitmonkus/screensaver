515ea1930d0f5bc471d6ed5ba689d0e2100a88c0
ironmonkus <ironmonkus@gmail.com>
2021-11-23 14:40:08 -0500

    Added CSS colors, index, and images

--- js/index.js
+++ js/index.js
@@ -1,10 +1,45 @@
 const screensaver = document.getElementById("screensaver");
 const orb = document.getElementById("orb");
 const countdown = document.getElementById("countdown");
+const imageEl = document.getElementById("image");
 
 // 5 second countdown timer
-let count = 5;
+let count;
 let timer;
+let idle;
+
+const chickenArray = [
+  "../img/chicken/1.JPG",
+  "../img/chicken/2.JPG",
+  "../img/chicken/3.JPG",
+  "../img/chicken/4.JPG",
+  "../img/chicken/5.JPG",
+];
+
+function imageArray() {
+  let imgNumber = 0;
+  let imgLength = chickenArray.length - 1;
+  console.log(imgNumber);
+  console.log(imgLength);
+  const nextEl = document.getElementById("next");
+  const backEl = document.getElementById("back");
+
+  nextEl.addEventListener("click", function () {
+    if (imgNumber < imgLength) {
+      imgNumber++;
+      imageEl.innerHTML = `<img src="${chickenArray[imgNumber]}" alt="house">`;
+      console.log(`Next button -- imgNumber = ${imgNumber}`);
+    }
+  });
+
+  backEl.addEventListener("click", function () {
+    if (imgNumber > 0) {
+      imgNumber--;
+      imageEl.innerHTML = `<img src="${chickenArray[imgNumber]}" alt ="house">`;
+      console.log(`Back button -- imgNumber = ${imgNumber}`);
+    }
+  });
+}
 
 // This function counts down from 5 - 0
 // Send the count to the paragraph inside the orb
@@ -19,27 +54,37 @@ function theCountDown() {
     clearInterval(timer);
     screensaver.style.animation = "screensaver 13s linear infinite alternate";
     orb.style.animation = "orb 7s linear infinite alternate";
-    countdown.textContent = "";
+    countdown.textContent = "STOP";
+    countdown.style.fontSize = "20px";
   }
 }
 // Call theCountDown function to start the timer,
 //counting down every 1000ms or 1 second
 function timerCount() {
+  count = 5;
   console.log(`From timerCount function.  count = ${count}`);
   timer = setInterval("theCountDown()", 1000);
 }
 // timer = setInterval("theCountDown()", 1000);
 
-window.addEventListener("mousemove", function () {
+orb.addEventListener("mousemove", function () {
   screensaver.style.animation = "screensaver 0s";
   orb.style.animation = "orb 0s";
+  // idleTime();
 });
 
 // NOT SURE YET.... trying to figure out idle...
-function timeOut() {
-  count = 5;
-  console.log(`From mousemove.  count = ${count}`);
-  setTimeout(timerCount, 2000);
-}
+// function idleTime() {
+//   window.removeEventListener("mousemove", null);
+//   console.log(`Starting Idle timer.  count = ${count}`);
+//   idle = 5;
+//   while (idle > 0) {
+//     idle--;
+//     if (idle === 0) {
+//       setInterval(timerCount, 5000);
+//     }
+//   }
+// }
 
 timerCount();
+imageArray();
